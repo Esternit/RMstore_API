9c0e4d3c3c949c653008f45ea70c39b55182d3b3
Esternit <92298236+Esternit@users.noreply.github.com>
2024-02-11 17:42:42 +0300

    bug fix

--- dbService.js
+++ dbService.js
@@ -55,7 +55,7 @@ class DbService {
                 connection.query(query, [id], (err, results) => {
                     if (err) reject(new Error(err.message));
                     for(let i = 0; i < results.length; i++){
-                        results[i]["start_price"] =  Math.round((results[i]["price"]*data["exchange"]+1900)*1.02 + 900);
+                        results[i]["start_price"] =  Math.round((results[i]["start_price"]*data["exchange"]+1900)*1.02 + 900);
                     }
                     resolve(results);
                 })
