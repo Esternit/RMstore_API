10f868a117485e309daa5c90a41faadd13cb3a19
karpov.dmitrii <dev@karpov.d.a3>
2024-02-16 11:17:57 +0300

    testing

--- dbService.js
+++ dbService.js
@@ -26,6 +26,7 @@ connection.query(
     'SELECT * FROM exchange',
     function(err, results, fields) {
       console.log(results["exchange_rate"]);
+      console.log(results);
       ex = results["exchange_rate"] // results contains rows returned by server// fields contains extra meta data about results, if available
     }
 );
