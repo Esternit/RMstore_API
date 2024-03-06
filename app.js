2d310ccce0254f74f8df27939385640311eaaa5f
Esternit <92298236+Esternit@users.noreply.github.com>
2024-03-06 19:03:31 +0300

    testing

--- app.js
+++ app.js
@@ -123,7 +123,8 @@ app.post('/searchDataFromStart/:name', (request, response) => {
 
 app.post('/sendMessage',(request, response) => {
     console.log(request.body);
-    let url = "https://api.telegram.org/bot7004894962:AAG-lllNmtfEE6Abh2RbkqETVN6y9cMEUtA/sendPhoto?chat_id=" + request.body["user_id"].toString() + "&photo=" + request.body["img"] + "&caption=" + request.body["title"];
+    let text = "Наименование: " + request.body["title"] +"\n Рамзер/цена: "+ request.body["size_name"] + "EU / " + request.body["pricing"] + "руб.";
+    let url = "https://api.telegram.org/bot7004894962:AAG-lllNmtfEE6Abh2RbkqETVN6y9cMEUtA/sendPhoto?chat_id=" + request.body["user_id"].toString() + "&photo=" + request.body["img"] + "&caption=" + text;
     console.log(url);
     https
     .get(url, (res) => {
